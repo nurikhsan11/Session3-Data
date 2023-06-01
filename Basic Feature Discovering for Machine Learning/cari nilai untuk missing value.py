7eb9cdabb448ac055c774726886e308663e62870
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-06-01 12:33:43 +0700

    belajar

---
+++ Basic Feature Discovering for Machine Learning/cari nilai untuk missing value.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def concat_df(train_data, test_data):
+	return pd.concat([train_data, test_data], sort=True).reset_index(drop=True)
+
+df_train = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_train.csv')
+df_test = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_test.csv')
+df_all = concat_df(df_train, df_test)
+df_train.name = 'Training Set'
+df_test.name = 'Test Set'
+df_all.name = 'All Set' 
+dfs = [df_train, df_test]
+
+age_by_pclass_sex = df_all.groupby(['Sex', 'Pclass']).median()['Age']
+
+for pclass in range(1, 4):
+	for sex in ['female', 'male']:
+		print('Median age of Pclass {} {}s: {}'.format(pclass, sex, age_by_pclass_sex[sex][pclass]))
+print('Median age of all passengers: {}'.format(df_all['Age'].median()))
+
+# Filling the missing values in Age with the medians of Sex and Pclass groups
+df_all['Age'] = df_all.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
---
+++ Basic Feature Discovering for Machine Learning/isi nilai kosong.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def concat_df(train_data, test_data):
+	return pd.concat([train_data, test_data], sort=True).reset_index(drop=True)
+
+df_train = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_train.csv')
+df_test = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_test.csv')
+df_all = concat_df(df_train, df_test)
+df_train.name = 'Training Set'
+df_test.name = 'Test Set'
+df_all.name = 'All Set' 
+dfs = [df_train, df_test]
+
+age_by_pclass_sex = df_all.groupby(['Sex', 'Pclass']).median()['Age']
+df_all['Age'] = df_all.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
+
+# Filling the missing values in Embarked with S
+df_all['Embarked'] = df_all['Embarked'].fillna('S')
+
+med_fare = df_all.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+# Filling the missing value in Fare with the median Fare of 3rd class alone passenger
+df_all['Fare'] = df_all['Fare'].fillna(med_fare)

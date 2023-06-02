0cec6d5bce36c7d888b7e24345713a84dba45d0a
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-06-02 17:01:32 +0700

    belajar

---
+++ Basic Feature Discovering for Machine Learning/Feature Engineering2.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+
+def concat_df(train_data, test_data):
+    return pd.concat([train_data, test_data], sort=True).reset_index(drop=True)
+
+df_train = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_train.csv')
+df_test = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_test.csv')
+df_all = concat_df(df_train, df_test)
+df_train.name = 'Training Set'
+df_test.name = 'Test Set'
+df_all.name = 'All Set' 
+dfs = [df_train, df_test]
+
+age_by_pclass_sex = df_all.groupby(['Sex', 'Pclass']).median()['Age']
+df_all['Age'] = df_all.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
+df_all['Embarked'] = df_all['Embarked'].fillna('S')
+med_fare = df_all.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+df_all['Fare'] = df_all['Fare'].fillna(med_fare)
+df_all['Fare'] = pd.qcut(df_all['Fare'], 13)
+df_all['Family_Size'] = df_all['SibSp'] + df_all['Parch'] + 1
+
+family_map = {1: 'Alone', 2: 'Small', 3: 'Small', 4: 'Small', 5: 'Medium', 6: 'Medium', 7: 'Large', 8: 'Large', 11: 'Large'}
+df_all['Family_Size_Grouped'] = df_all['Family_Size'].map(family_map)
+
+df_all['Ticket_Frequency'] = df_all.groupby('Ticket')['Ticket'].transform('count')
+
+fig, axs = plt.subplots(figsize=(12, 9))
+sns.countplot(x='Ticket_Frequency', hue='Survived', data=df_all)
+
+plt.xlabel('Ticket Frequency', size=15, labelpad=20)
+plt.ylabel('Passenger Count', size=15, labelpad=20)
+plt.tick_params(axis='x', labelsize=15)
+plt.tick_params(axis='y', labelsize=15)
+
+plt.legend(['Not Survived', 'Survived'], loc='upper right', prop={'size': 15})
+plt.title('Count of Survival in {} Feature'.format('Ticket Frequency'), size=15, y=1.05)
+plt.tight_layout()
+plt.show()

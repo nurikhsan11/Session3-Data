a30d0040c973962943d1009edf375500f86342cb
nurikhsan11 <nurikhsannn2003@gmail.com>
2023-06-08 09:09:51 +0700

    belajar

---
+++ Basic Feature Discovering for Machine Learning/Modelling dengan hasil Feature Engineering - Part 3.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+
+def concat_df(train_data, test_data):
+    return pd.concat([train_data, test_data], sort=True).reset_index(drop=True)
+
+df_train = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_train.csv')
+df_test = pd.read_csv('https://storage.googleapis.com/dqlab-dataset/challenge/feature-engineering/titanic_test.csv')
+df_all = concat_df(df_train, df_test)
+df_train.name = 'Training Set'
+df_test.name = 'Test Set'
+df_all.name = 'All Set'
+
+age_by_pclass_sex = df_all.groupby(['Sex', 'Pclass']).median()['Age']
+df_all['Age'] = df_all.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
+df_all['Embarked'] = df_all['Embarked'].fillna('S')
+med_fare = df_all.groupby(['Pclass', 'Parch', 'SibSp']).Fare.median()[3][0][0]
+df_all['Fare'] = df_all['Fare'].fillna(med_fare)
+df_all['Fare'] = pd.qcut(df_all['Fare'], 13)
+df_all['Family_Size'] = df_all['SibSp'] + df_all['Parch'] + 1
+
+family_map = {1: 'Alone', 2: 'Small', 3: 'Small', 4: 'Small', 5: 'Medium', 6: 'Medium', 7: 'Large', 8: 'Large', 11: 'Large'}
+df_all['Family_Size_Grouped'] = df_all['Family_Size'].map(family_map)
+df_all['Ticket_Frequency'] = df_all.groupby('Ticket')['Ticket'].transform('count')
+df_all['Title'] = df_all['Name'].str.split(', ', expand=True)[1].str.split('.', expand=True)[0]
+df_all['Is_Married'] = 0
+df_all['Is_Married'].loc[df_all['Title'] == 'Mrs'] = 1
+df_all['Title'] = df_all['Title'].replace(['Miss', 'Mrs','Ms', 'Mlle', 'Lady', 'Mme', 'the Countess', 'Dona'], 'Miss/Mrs/Ms')
+df_all['Title'] = df_all['Title'].replace(['Dr', 'Col', 'Major', 'Jonkheer', 'Capt', 'Sir', 'Don', 'Rev'], 'Dr/Military/Noble/Clergy')
+
+df_train = df_all.loc[:890]
+df_test = df_all.loc[891:]
+dfs = [df_train, df_test]
+
+non_numeric_features = ['Embarked', 'Sex', 'Title', 'Family_Size_Grouped', 'Age', 'Fare']
+
+for df in dfs:
+    for feature in non_numeric_features:        
+        df[feature] = LabelEncoder().fit_transform(df[feature])
+        
+cat_features = ['Pclass', 'Sex', 'Embarked', 'Title', 'Family_Size_Grouped']
+encoded_features = []
+
+for df in dfs:
+    for feature in cat_features:
+        encoded_feat = OneHotEncoder().fit_transform(df[feature].values.reshape(-1, 1)).toarray()
+        n = df[feature].nunique()
+        cols = ['{}_{}'.format(feature, n) for n in range(1, n + 1)]
+        encoded_df = pd.DataFrame(encoded_feat, columns=cols)
+        encoded_df.index = df.index
+        encoded_features.append(encoded_df)
+
+df_train = pd.concat([df_train, *encoded_features[:5]], axis=1)
+df_test = pd.concat([df_test, *encoded_features[5:]], axis=1)
+drop_cols = ['Cabin', 'Embarked', 'Family_Size', 'Family_Size_Grouped', 'Survived',
+             'Name', 'PassengerId', 'Pclass', 'Sex', 'Ticket', 'Title']
+
+# Silakan ketikkan kodenya di sini
+X_train = StandardScaler().fit_transform(df_train.drop(columns=drop_cols))
+y_train = df_train['Survived'].values
+X_test = StandardScaler().fit_transform(df_test.drop(columns=drop_cols))
+
+print('X_train shape: {}'.format(X_train.shape))
+print('y_train shape: {}'.format(y_train.shape))
+print('X_test shape: {}'.format(X_test.shape))
